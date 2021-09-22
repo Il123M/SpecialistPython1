f4ef648880b771f0557534c6d9284606a07435d2
Il123M <91018524+Il123M@users.noreply.github.com>
2021-09-22 16:54:17 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,15 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+number = int(input("Введите число: "))
+a=number%3
+b=number%5
+if a==0:
+ if b==0:
+  print ("FOOBAR")
+ else:
+    print ("BAR")
+elif b==0:
+    print ("FOO")
+else:
+    print ("NONE")
